aafce66452614047eaf34f6db03ee1b72f89a8a6
ajayjangid00 <ajayjangid163@gmail.com>
2023-08-17 18:05:01 +0530

    First commit
    First Date 17-18-2023

---
+++ js/style.js
@@ -0,0 +1,39 @@
+
+        $(".carousel").owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+        0:{
+        items:1,
+        nav: false
+        },
+        150:{
+        items:2,
+        nav: false
+        },
+        300:{
+        items:3,
+        nav: false
+        },
+        450:{
+        items:4,
+        nav: false
+        },
+        600:{
+        items:5,
+        nav: false
+        },
+        750:{
+        items:6,
+        nav: false
+        },
+        1000:{
+        items:7,
+        nav: false
+        }
+        }
+        });
+       
